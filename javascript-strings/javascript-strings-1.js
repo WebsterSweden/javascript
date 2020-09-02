c4294138a3ccb414fdd3ebf5434b05f6a82e3ad7
WebsterSweden <webster@webster.se>
2020-09-02 17:55:01 +0200

    Edit javascript-strings-1.html.

--- javascript-strings/javascript-strings-1.js
+++ javascript-strings/javascript-strings-1.js
@@ -5,4 +5,16 @@ browser, devel
 var stringOne = "This is string number one";
 var stringTwo = "This is string number two";
 
-console.log(stringOne.length);
+console.log(stringOne + " = " + stringOne.length);
+
+var textInnerHtml = document.getElementById("textSampleOne").innerHTML;
+
+console.log(textInnerHtml);
+
+var textInnerText = document.getElementById("textSampleOne").innerText;
+
+console.log(textInnerText);
+
+var textTextContent = document.getElementById("textSampleOne").textContent;
+
+console.log(textTextContent);
