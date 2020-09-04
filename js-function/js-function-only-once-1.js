83e7299de8837a0899ab81a61a0200e96297b60a
WebsterSweden <webster@webster.se>
2020-09-04 22:44:57 +0200

    Edit js-function-only-once-1.html, js-function-only-once-1.js.

--- js-function/js-function-only-once-1.js
+++ js-function/js-function-only-once-1.js
@@ -4,3 +4,54 @@ browser, devel, es6
 /*global
 window
 */
+
+var onlyOnceOne = (function () {
+    'use strict';
+    var executed = false;
+    console.log("executed: " + executed);
+    return function () {
+        if (!executed) {
+            executed = true;
+            console.log("executed: " + executed);
+        }
+    };
+}());
+
+document.getElementById("onlyOnceOne").addEventListener("click", onlyOnceOne);
+
+var randomNumber = (function () {
+    'use strict';
+    var randomValue;
+    console.log("Random before: " + randomValue);
+    return function (flagForceNewValue) {
+        if (randomValue === undefined || flagForceNewValue) {
+            randomValue = Math.floor(Math.random() * (10 - 1) + 1);
+            document.getElementById("randomNumberOutput").innerText = randomValue;
+            console.log("Random after: " + randomValue);
+        }
+        console.log("Random last: " + randomValue);
+        return randomValue;
+    };
+}());
+
+// randomNumber();
+
+document.getElementById("randomNumber").addEventListener("click", randomNumber);
+
+var statusCheck = true;
+
+console.log("statusCheck before function: " + statusCheck);
+
+function onlyOnceTwo() {
+    'use strict';
+    console.log("statusCheck first in function: " + statusCheck);
+    if (statusCheck === true) {
+        console.log("statusCheck if true: " + statusCheck);
+        document.getElementById("textOne").disabled = true;
+        statusCheck = false;
+    } else {
+        console.log("statusCheck if else false: " + statusCheck);
+    }
+}
+
+document.getElementById("formOneSubmit").addEventListener("click", onlyOnceTwo);
