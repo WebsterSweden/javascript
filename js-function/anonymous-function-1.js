a3bdec34ceb074d6c309d8518fb69d534cb02189
WebsterSweden <webster@webster.se>
2020-09-28 20:38:46 +0200

    Edit anonymous-function-1.html, anonymous-function-1.js.

--- js-function/anonymous-function-1.js
+++ js-function/anonymous-function-1.js
@@ -5,4 +5,18 @@ browser, devel, es6
 window
 */
 
-console.log(document.currentScript);
+// console.log(document.currentScript);
+
+var anonymousFunctionOneVariable = function () {
+    'use strict';
+    console.log("anonymousFunctionOne no name function.");
+};
+
+document.getElementById("anonymousFunctionOneButton").addEventListener("click", anonymousFunctionOneVariable);
+
+var anonymousFunctionTwoVariable = function anonymousFunctionTwo() {
+    'use strict';
+    console.log("anonymousFunctionTwo named function.");
+};
+
+document.getElementById("anonymousFunctionTwoButton").addEventListener("click", anonymousFunctionTwoVariable);
