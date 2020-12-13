2d7d80e5ea81c63cdbb4483a43ad7adecd75200c
WebsterSweden <webster@webster.se>
2020-12-13 21:06:14 +0100

    Edit js-currentscript.

--- js-currentscript/js-currentscript-1.js
+++ js-currentscript/js-currentscript-1.js
@@ -8,3 +8,10 @@ window
 // bitwise, browser, convert, couch, devel, es6, eval, fudge, getset, long, node, single, this, white
 
 console.log(document.currentScript);
+
+let currentScriptElementOne = document.currentScript;
+
+// [objcet HTMLScriptelement]
+// document.getElementById("currentScriptOne").innerText = currentScriptElementOne;
+
+document.getElementById("currentScriptTwo").innerText = currentScriptElementOne.src;
