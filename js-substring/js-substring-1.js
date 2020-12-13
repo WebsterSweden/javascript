0f9fd856c6f58f7c275b7415c141f3ea12477c1f
WebsterSweden <webster@webster.se>
2020-12-13 20:38:05 +0100

    Edit js-substring.

--- js-substring/js-substring-1.js
+++ js-substring/js-substring-1.js
@@ -8,3 +8,20 @@ window
 // bitwise, browser, convert, couch, devel, es6, eval, fudge, getset, long, node, single, this, white
 
 console.log(document.currentScript);
+
+let stringOne = "The quick brown fox jumps over the lazy dog";
+
+document.getElementById("stringOne").innerText = stringOne;
+
+let substringOne = stringOne.substring();
+
+document.getElementById("substringOne").innerText = substringOne;
+
+// let substringTwo = stringOne.substring(0, 0);
+// let substringTwo = stringOne.substring(0);
+// let substringTwo = stringOne.substring(0, 3);
+// let substringTwo = stringOne.substring(3, 0);
+let substringTwo = stringOne.substring(0, stringOne.length);
+// let substringTwo = stringOne.substring(-6, stringOne.length + 6);
+
+document.getElementById("substringTwo").innerText = substringTwo;
